7b076901affada97ffcbbc7f667cea33a2719f46
RR3Z <consicration@mail.ru>
2023-04-30 17:54:35 +0300

    Сделал семестровую работу

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def create_connection(host_name, user_name, user_password, db_name):
 if __name__ == '__main__':
     # Открыть доступ к БД
     dbConnection = create_connection("localhost", "root", "951753swdQ", "db")
-    myСursor = dbConnection.cursor()
 
     # Вводное сообщение
-    print("1 - Получить информацию о заказах с информацией о покупателях\n"
+    print("Запросы из ЛР6:\n"
+          "1 - Получить информацию о заказах с информацией о покупателях\n"
           "2 - Получить информацию об играх\n"
           "3 - Добавить жанр в таблицу genres\n"
           "4 - Добавить новый заказ для пользователя по ID\n"
@@ -36,39 +36,63 @@ if __name__ == '__main__':
           "7 - Удалить жанр по названию\n"
           "8 - Удалить разработчика по названию\n"
           "9 - Аналитический запрос №1 - Получить список всех пользователей и количество их заказов с разбивкой по месяцам за последний год\n"
-          "10 - Аналитический запрос №2 - Получить список всех игр, у которых есть скидки в данный момент и указание на текущую скидку (по процентам и по цене в рублях)\n")
+          "10 - Аналитический запрос №2 - Получить список всех игр, у которых есть скидки в данный момент и указание на текущую скидку (по процентам и по цене в рублях)\n"
+          "\nЗапросы для семестровой работы:\n"
+          "11 - Получить информацию обо всех зарегистрированных пользователях\n"
+          "12 - Добавить нового пользователя в таблицу users\n"
+          "13 - Изменить информацию о пользователе по полю login\n"
+          "14 - Удалить пользователя из таблицы users\n"
+          "15 - Добавить игру к заказу (связь: многие ко многим)\n"
+          "16 - Заменить игру в заказе (связь: многие ко многим)\n"
+          "17 - Удалить игру из заказа (связь: многие ко многим)\n"
+          "18 - Аналитический запрос - Получить список всех заказов, в которых содержится игра с заданным названием\n")
 
     # Узнать, какой запрос хочет выполнить пользователь
     userChoice = int(input("Ваш выбор: "))
 
     # Проверка на значение, которое вводить пользователь
-    if userChoice < 1 or userChoice > 10:
+    if userChoice < 1 or userChoice > 18:
         print(f"\nЗначения '{userChoice}' нет в списке выбора")
     else:
         # Вызвать запрос, который попросил пользователь
         if userChoice == 1:
-            queries.readOrdersData(myСursor)
+            queries.getOrdersData(dbConnection)
         elif userChoice == 2:
-            queries.readPcGamesData(myСursor)
+            queries.getPcGamesData(dbConnection)
         elif userChoice == 3:
-            queries.addGenre(myСursor, dbConnection)
+            queries.addGenre(dbConnection)
         elif userChoice == 4:
-            queries.addOrder(myСursor, dbConnection)
+            queries.addOrder(dbConnection)
         elif userChoice == 5:
-            queries.updateDeveloperName(myСursor, dbConnection)
+            queries.updateDeveloperName(dbConnection)
         elif userChoice == 6:
-            queries.updatePcGameInfo(myСursor, dbConnection)
+            queries.updatePcGameInfo(dbConnection)
         elif userChoice == 7:
-            queries.deleteGenre(myСursor, dbConnection)
+            queries.deleteGenre(dbConnection)
         elif userChoice == 8:
-            queries.deleteDeveloper(myСursor, dbConnection)
+            queries.deleteDeveloper(dbConnection)
         elif userChoice == 9:
-            queries.getUsersOrders(myСursor)
+            queries.getUsersOrders(dbConnection)
         elif userChoice == 10:
-            queries.getDiscountedGames(myСursor)
+            queries.getDiscountedGames(dbConnection)
+        elif userChoice == 11:
+            queries.getUsersData(dbConnection)
+        elif userChoice == 12:
+            queries.addNewUser(dbConnection)
+        elif userChoice == 13:
+            queries.updateUserData(dbConnection)
+        elif userChoice == 14:
+            queries.deleteUser(dbConnection)
+        elif userChoice == 15:
+            queries.addGameToOrder(dbConnection)
+        elif userChoice == 16:
+            queries.updateGameInOrder(dbConnection)
+        elif userChoice == 17:
+            queries.deleteGameFromOrder(dbConnection)
+        elif userChoice == 18:
+            queries.getOrdersByGameName(dbConnection)
 
     # Закрыть доступ к БД
-    myСursor.close()
     dbConnection.close()
 
 
--- mySqlQueries.py
+++ mySqlQueries.py
@@ -1,6 +1,8 @@
 # ПОЛУЧЕНИЕ ДАННЫХ
 # Получить информацию о заказах с информацией о покупателях
-def readOrdersData(myCursor):
+def getOrdersData(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Обратиться к БД с запросом
     readOrdersDataQuery = """
                           SELECT users.login, orders.product_units, orders.order_date
@@ -12,8 +14,12 @@ def readOrdersData(myCursor):
     for dbData in myCursor:
         print(dbData)
 
+    myCursor.close()
+
 # Получить название игры, ее стоимость, ее разработчика(developer)
-def readPcGamesData(myCursor):
+def getPcGamesData(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Обратиться к БД с запросом
     readPcGamesDataQuery = """
                     SELECT pc_games.name, pc_games.price, developers.company_name
@@ -24,23 +30,31 @@ def readPcGamesData(myCursor):
     for dbData in myCursor:
         print(dbData)
 
+    myCursor.close()
+
 
 # ДОБАВЛЕНИЕ ДАННЫХ
 # Добавить жанр в таблицу genres
-def addGenre(myCursor, dbConnection):
+def addGenre(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Получить данные от пользователя
     genreName = input("Имя жанра: ")
     genreDesc = input("Описание жанра: ")
     genre = (genreName, genreDesc)
 
     # Обратиться к БД с запросом
-    addGenreQuery = "INSERT INTO genres (name, description) VALUES (%s,%s);"
+    addGenreQuery = "INSERT INTO genres (name, description) VALUES ('%s','%s');"
 
     myCursor.execute(addGenreQuery % genre)
     dbConnection.commit()
 
-# Добавить новый заказ для пользователя по ID
-def addOrder(myCursor, dbConnection):
+    myCursor.close()
+
+# Добавить новый заказ пользователя по ID
+def addOrder(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Получить данные от пользователя
     orderDate = input("Введите дату заказа (YYYY-MM-DD HH:MM:SS): ")
     productUnits = int(input("Введите кол-во доступных единиц товара: "))
@@ -52,16 +66,20 @@ def addOrder(myCursor, dbConnection):
     myCursor.execute(addOrderQuery % order)
     dbConnection.commit()
 
+    myCursor.close()
+
 
 # ИЗМЕНЕНИЕ ДАННЫХ
 # Изменить имя компании разработчика по id
-def updateDeveloperName(myCursor, dbConnection):
+def updateDeveloperName(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Получить от пользователя id компании, чье имя хотим изменить
     idDeveloper = int(input("Введите id компании, чье имя хотите изменить: "))
 
     # Проверить имеется ли хотя бы одна запись с заданным именем в таблице
-    checkDeveloperId = "SELECT id_developers FROM developers WHERE id_developers = %d;"
-    myCursor.execute(checkDeveloperId % idDeveloper)
+    checkDeveloperIdQuery = "SELECT id_developers FROM developers WHERE id_developers = %d;"
+    myCursor.execute(checkDeveloperIdQuery % idDeveloper)
     result = myCursor.fetchone()
 
     if result:
@@ -77,14 +95,18 @@ def updateDeveloperName(myCursor, dbConnection):
         # Вывести ошибку, что нет полей с заданным пользователем id
         print(f"В таблице developers нет разработчика с id {idDeveloper}")
 
+    myCursor.close()
+
 # Изменить информацию об игре по имени
-def updatePcGameInfo(myCursor, dbConnection):
+def updatePcGameInfo(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Получить название игры, чью информацию мы хотим изменить
     gameName = input("Название игры: ")
 
     # Проверить имеется ли хотя бы одна запись с заданным именем в таблице
-    checkPcGameName = "SELECT * FROM pc_games WHERE name = '%s';"
-    myCursor.execute(checkPcGameName % gameName)
+    checkPcGameNameQuery = "SELECT * FROM pc_games WHERE name = '%s';"
+    myCursor.execute(checkPcGameNameQuery % gameName)
     result = myCursor.fetchone()
 
     if result:
@@ -121,15 +143,19 @@ def updatePcGameInfo(myCursor, dbConnection):
         # Вывести ошибку, что в таблице с играми нет игры с заданным пользователем именем
         print(f"В таблице pc_games нет игры с именем {gameName}")
 
+    myCursor.close()
+
 
 # УДАЛЕНИЕ ДАННЫХ
 # Удалить жанр по названию
-def deleteGenre(myCursor, dbConnection):
+def deleteGenre(dbConnection):
+    myCursor = dbConnection.cursor()
+
     genreName = input("Введите название жанра: ")
 
     # Проверить имеется ли хотя бы одна запись с данным именем жанра в таблице
-    checkGenreName = "SELECT * FROM genres WHERE name = '%s';"
-    myCursor.execute(checkGenreName % genreName)
+    checkGenreNameQuery = "SELECT * FROM genres WHERE name = '%s';"
+    myCursor.execute(checkGenreNameQuery % genreName)
     result = myCursor.fetchone()
 
     if result:
@@ -140,13 +166,17 @@ def deleteGenre(myCursor, dbConnection):
     else:
         print(f"В таблице genres нет жанра с именем {genreName}")
 
+    myCursor.close()
+
 # Удалить разработчика по названию
-def deleteDeveloper(myCursor, dbConnection):
+def deleteDeveloper(dbConnection):
+    myCursor = dbConnection.cursor()
+
     developerName = input("Введите имя компании разработчика: ")
 
     # Проверить имеется ли хотя бы одна запись с данным именем компании в таблице
-    checkDeveloperName = "SELECT * FROM developers WHERE company_name = '%s';"
-    myCursor.execute(checkDeveloperName % developerName)
+    checkDeveloperNameQuery = "SELECT * FROM developers WHERE company_name = '%s';"
+    myCursor.execute(checkDeveloperNameQuery % developerName)
     result = myCursor.fetchone()
 
     if result:
@@ -157,10 +187,14 @@ def deleteDeveloper(myCursor, dbConnection):
     else:
         print(f"В таблице developers нет поля с именем {developerName}")
 
+    myCursor.close()
+
 
 # Аналитические запросы
-# Запрос №1 : Получить список всех пользователей и количество их заказов с разбивкой по месяцам за последний год
-def getUsersOrders(myCursor):
+# Аналитический запрос №1: Получить список всех пользователей и количество их заказов с разбивкой по месяцам за последний год
+def getUsersOrders(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Обратиться к БД с запросом
     getUsersOrdersQuery = """
             SELECT 
@@ -190,8 +224,12 @@ def getUsersOrders(myCursor):
     else:
         print("За последний год не было совершено заказов")
 
-# Запрос №2: Получить список всех игр, у которых есть скидки в данный момент и указание на текущую скидку (по процентам и по цене в рублях)
-def getDiscountedGames(myCursor):
+    myCursor.close()
+
+# Аналитический запрос №2: Получить список всех игр, у которых есть скидки в данный момент и указание на текущую скидку (по процентам и по цене в рублях)
+def getDiscountedGames(dbConnection):
+    myCursor = dbConnection.cursor()
+
     # Обратиться к БД с запросом
     getDiscountedGamesQuery = """
                             SELECT pc_games.name, pc_games.price, discounts.discount_percentage, discounts.discount_price
@@ -208,3 +246,253 @@ def getDiscountedGames(myCursor):
             print(game)
     else:
         print("Игр со скидками не имеется")
+
+    myCursor.close()
+
+
+
+# Запросы для семестровой работы
+# Получить информацию обо всех зарегистрированных пользователях
+def getUsersData(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Обратиться к БД с запросом
+    getUsersDataQuery = "SELECT * FROM users"
+    myCursor.execute(getUsersDataQuery)
+    for result in myCursor:
+        print(result)
+
+    myCursor.close()
+
+# Добавить нового пользователя в таблицу users
+def addNewUser(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить данные от пользователя
+    userLogin = input("Логин пользователя: ")
+    userPassword = input("Пароль пользователя: ")
+    userNickname = input("Псевдоним пользователя: ")
+    userMail = input("Почта пользователя: ")
+    userInfo = (userLogin, userPassword, userNickname, userMail)
+
+    # Обратиться к БД с запросом
+    addNewUserQuery = "INSERT INTO users (login, password, nickname, mail, registration_date) VALUES ('%s','%s','%s','%s',NOW());"
+
+    myCursor.execute(addNewUserQuery % userInfo)
+    dbConnection.commit()
+
+    myCursor.close()
+
+# Изменить информацию о пользователе по полю login
+def updateUserData(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить от пользователя login
+    userLogin = int(input("Введите login пользователя: "))
+
+    # Проверить имеется ли хотя бы одна запись с заданным login в таблице users
+    checkUserLoginQuery = "SELECT login FROM users WHERE login = %s;"
+    myCursor.execute(checkUserLoginQuery % userLogin)
+    result = myCursor.fetchone()
+
+    if result:
+        # Получить новые данные от пользователя
+        newUserLogin = input("Введите новый логин пользователя: ")
+        newUserPassword = input("Введите новый пароль пользователя: ")
+        newUserNickname = input("Введите новый псевдоним пользователя: ")
+        newUserMail = input("Введите новую почту пользователя: ")
+        userData = (newUserLogin, newUserPassword, newUserNickname, newUserMail, userLogin)
+
+        # Изменить информацию о пользователе
+        updateUserDataQuery = """
+                            UPDATE users 
+                            SET login = '%s', 
+                                password = '%s', 
+                                nickname = '%s', 
+                                mail = '%s' 
+                            WHERE login = '%s';
+        """
+        myCursor.execute(updateUserDataQuery % userData)
+        dbConnection.commit()
+    else:
+        # Вывести ошибку, что нет полей с заданным login в таблице users
+        print(f"В таблице users нет пользователя с login '{userLogin}'")
+
+    myCursor.close()
+
+# Удалить пользователя из таблицы users
+def deleteUser(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить login пользователя
+    userLogin = input("Введите login пользователя: ")
+
+    # Проверить имеется ли хотя бы одна запись с заданным login в таблице users
+    checkUserLoginQuery = "SELECT * FROM users WHERE login = '%s';"
+    myCursor.execute(checkUserLoginQuery % userLogin)
+    checkUserLoginResult = myCursor.fetchone()
+
+    if checkUserLoginResult:
+        # Обратиться к БД с запросом
+        deleteUserQuery = "DELETE FROM users WHERE login = '%s';"
+        myCursor.execute(deleteUserQuery % userLogin)
+        dbConnection.commit()
+    else:
+        print(f"В таблице users нет пользователя с логином '{userLogin}'")
+
+    myCursor.close()
+
+
+# Работа со связью многие-ко-многим
+# Добавить игру в заказ
+def addGameToOrder(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить данные от пользователя
+    idOrder = int(input("Номер заказа: "))
+    gameName = input("Название игры: ")
+
+    # Проверить имеется ли запись с заданным id в таблице
+    checkIdOrderQuery = "SELECT * FROM orders WHERE id_orders = %d;"
+    myCursor.execute(checkIdOrderQuery % idOrder)
+    checkIdOrderResult = myCursor.fetchone()
+
+    if checkIdOrderResult:
+        # Проверить имеется ли запись с заданной игрой в таблице pc_games
+        checkGameNameQuery = "SELECT * FROM pc_games WHERE name = '%s';"
+        myCursor.execute(checkGameNameQuery % gameName)
+        checkGameNameResult = myCursor.fetchone()
+
+        if checkGameNameResult:
+            # Обратиться к БД с запросом
+            addGameToOrderQuery = "INSERT INTO pc_games_to_orders (id_games, id_orders) VALUES ((SELECT id_games FROM pc_games WHERE name = '%s'), %d);"
+
+            gameInOrder = (gameName, idOrder)
+            myCursor.execute(addGameToOrderQuery % gameInOrder)
+            dbConnection.commit()
+        else:
+            print(f"Игры с названием '{gameName}' не существует")
+    else:
+        print(f"Заказа с номер '{idOrder}' не существует")
+
+    myCursor.close()
+
+# Заменить игру в заказе
+def updateGameInOrder(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить номер заказа у пользователя
+    idOrder = int(input("Номер заказа: "))
+
+    # Проверить имеется ли запись с заданным номером заказа в таблице orders
+    checkIdOrderQuery = "SELECT * FROM orders WHERE id_orders = %d;"
+    myCursor.execute(checkIdOrderQuery % idOrder)
+    checkIdOrderResult = myCursor.fetchone()
+
+    if checkIdOrderResult:
+        gameName = input("Название игры, которую хотите заменить: ")
+
+        # Проверить имеется ли запись с заданной игрой в таблице pc_games_to_orders
+        checkGameNameInOrderQuery = "SELECT * FROM pc_games_to_orders WHERE id_orders = %d AND id_games = (SELECT id_games FROM pc_games WHERE name = '%s');"
+        myCursor.execute(checkGameNameInOrderQuery % (idOrder, gameName))
+        checkGameNameInOrderResult = myCursor.fetchone()
+
+        if checkGameNameInOrderResult:
+            newGameName = input("Название игры, которую хотите добавить: ")
+
+            # Проверить имеется ли запись с новой заданной игрой в таблице pc_games
+            checkNewGameNameQuery = "SELECT * FROM pc_games WHERE id_games = (SELECT id_games FROM pc_games WHERE name = '%s');"
+            myCursor.execute(checkNewGameNameQuery % newGameName)
+            checkNewGameNameResult = myCursor.fetchone()
+
+            if checkNewGameNameResult:
+                gameInOrder = (newGameName, idOrder, gameName)
+
+                # Обратиться к БД с запросом
+                updateGameToOrderQuery = "UPDATE pc_games_to_orders SET id_games = (SELECT id_games FROM pc_games WHERE name = '%s') WHERE id_orders = %d AND id_games = (SELECT id_games FROM pc_games WHERE name = '%s');"
+                myCursor.execute(updateGameToOrderQuery % gameInOrder)
+                dbConnection.commit()
+            else:
+                print(f"Игры с названием '{newGameName}' не существует")
+        else:
+            print(f"Игры с названием '{gameName}' в заказе с номер '{idOrder}' не имеется")
+    else:
+        print(f"Заказа с номер '{idOrder}' не существует")
+
+    myCursor.close()
+
+# Удалить игру из заказа
+def deleteGameFromOrder(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить номер заказа у пользователя
+    idOrder = int(input("Номер заказа: "))
+
+    # Проверить имеется ли запись с заданным номером заказа в таблице orders
+    checkIdOrderQuery = "SELECT * FROM orders WHERE id_orders = %d;"
+    myCursor.execute(checkIdOrderQuery % idOrder)
+    checkIdOrderResult = myCursor.fetchone()
+
+    if checkIdOrderResult:
+        gameName = input("Название игры, которую хотите удалить: ")
+
+        checkGameNameQuery = "SELECT id_games FROM pc_games WHERE name = '%s';"
+        myCursor.execute(checkGameNameQuery % gameName)
+        checkGameNameResult = myCursor.fetchone()
+
+        if checkGameNameResult:
+            # Проверить имеется ли запись с заданной игрой в таблице pc_games_to_orders
+            checkGameNameInOrderQuery = "SELECT * FROM pc_games_to_orders WHERE id_orders = %d AND id_games = (SELECT id_games FROM pc_games WHERE name = '%s');"
+            myCursor.execute(checkGameNameInOrderQuery % (idOrder, gameName))
+            checkGameNameInOrderResult = myCursor.fetchone()
+
+            if checkGameNameInOrderResult:
+                    gameInOrder = (idOrder, gameName)
+
+                    # Обратиться к БД с запросом
+                    updateGameToOrderQuery = "DELETE FROM pc_games_to_orders WHERE id_orders = %d AND id_games = (SELECT id_games FROM pc_games WHERE name = '%s');"
+                    myCursor.execute(updateGameToOrderQuery % gameInOrder)
+                    dbConnection.commit()
+            else:
+                print(f"Игры с названием '{gameName}' в заказе с номер '{idOrder}' не имеется")
+        else:
+            print(f"Игры с названием '{gameName}' не существует")
+    else:
+        print(f"Заказа с номер '{idOrder}' не существует")
+
+    myCursor.close()
+
+# Аналитический запрос: Получить список всех заказов, в которых содержится игра с заданным названием
+def getOrdersByGameName(dbConnection):
+    myCursor = dbConnection.cursor()
+
+    # Получить название игры у пользователя
+    gameName = input("Введите название игры: ")
+
+    # Проверить, имеется ли игра с заданным именем в таблице pc_games
+    checkGameNameQuery = "SELECT id_games FROM pc_games WHERE name = '%s';"
+    myCursor.execute(checkGameNameQuery % gameName)
+    checkGameNameResult = myCursor.fetchone()
+
+    if checkGameNameResult:
+        # Обратиться к БД с запросом
+        getOrdersDataByGameNameQuery = """
+                        SELECT users.nickname, orders.id_orders, orders.order_date FROM pc_games
+                        JOIN pc_games_to_orders ON pc_games.id_games = pc_games_to_orders.id_games
+                        JOIN orders ON pc_games_to_orders.id_orders = orders.id_orders
+                        JOIN users ON orders.id_users = users.id_users
+                        WHERE pc_games.name = '%s';
+        """
+
+        myCursor.execute(getOrdersDataByGameNameQuery % gameName)
+        ordersDataByGameNameResult = myCursor.fetchall()
+
+        # Вывести результат выполнения запроса
+        if ordersDataByGameNameResult:
+            for result in ordersDataByGameNameResult:
+                print(f"User: {result[0]}, Order ID: {result[1]}, Order date: {result[2]}")
+        else:
+            print(f"Ни один человек игру '{gameName}' не приобрел")
+    else:
+        print(f"Игры с названием '{gameName}' не существует")
+    myCursor.close()
